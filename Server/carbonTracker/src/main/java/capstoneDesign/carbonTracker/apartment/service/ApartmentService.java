0874bfe0ba611516072f9966b692b99980f88dd5
SeongukBaek <bsu6235@gmail.com>
2022-05-11 12:47:14 +0900

    #19 feature: create apt & coordinate by using apis

--- Server/carbonTracker/src/main/java/capstoneDesign/carbonTracker/apartment/service/ApartmentService.java
+++ Server/carbonTracker/src/main/java/capstoneDesign/carbonTracker/apartment/service/ApartmentService.java
@@ -4,9 +4,15 @@ import capstoneDesign.carbonTracker.apartment.dto.AptEnergyRequest;
 import capstoneDesign.carbonTracker.apartment.dto.AptListRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -22,9 +28,8 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -43,6 +48,21 @@ public class ApartmentService {
     @Value("${aptEnergyKey}")
     private String aptEnergyKey;
 
+    @Value("${aptBasicInfoUrl}")
+    private String aptBasicInfoUrl;
+
+    @Value("${aptBasicInfoKey}")
+    private String aptBasicInfoKey;
+
+    @Value("${vWorldUrl}")
+    private String vWorldUrl;
+
+    @Value("${vWorldKey}")
+    private String vWorldKey;
+
+    private int noneCount = 0;
+    private final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+
     public String aptLists(AptListRequest aptListRequest) throws Exception {
         String urlBuilder = aptListUrl +
                 "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + aptListKey + /*Service Key*/
@@ -51,8 +71,7 @@ public class ApartmentService {
                 "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(aptListRequest.getCount()), "UTF-8"); /*목록 건수*/
         String attributeList = "";
 
-        //return callApi(urlBuilder, "aptLists", attributeList);
-        return "";
+        return callApi(urlBuilder, "aptLists", "df", attributeList);
     }
 
     public String aptEnergy(AptEnergyRequest aptEnergyRequest) throws Exception {
@@ -65,55 +84,162 @@ public class ApartmentService {
         return callApi(urlBuilder, "aptEnergy", aptEnergyRequest.getDate(), attributeList);
     }
 
-    public void aptListAll() throws Exception {
-        String urlBuilder = aptListUrl +
-                "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + aptListKey + /*Service Key*/
-                "&" + URLEncoder.encode("sidoCode", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(27), "UTF-8") + /*시도코드*/
-                "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(1), "UTF-8") + /*페이지번호*/
-                "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(943), "UTF-8"); /*목록 건수*/
-        String attributeList = "";
+    public String aptListAll() throws Exception {
+        // API 호출 결과가 없을 때까지 단지를 1개씩 받아 좌표 변환을 수행한다. 더 이상 받을 수 없는 경우 <item>이 비어있을 것
+        // 결과는 일단 toDB에 append한다.
+        StringBuilder toDB = new StringBuilder();
+        int idx = 1;
 
-        //callApi(urlBuilder, "aptListAll", attributeList);
+        while (true) {
+            String reqBuilder = aptListUrl +
+                    "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + aptListKey + /*Service Key*/
+                    "&" + URLEncoder.encode("sidoCode", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(27), "UTF-8") + /*시도코드*/
+                    "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(idx), "UTF-8") + /*페이지번호*/
+                    "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode(String.valueOf(1), "UTF-8"); /*목록 건수*/
+
+            String[] toDB1 = getApt(reqBuilder);
+            // 호출 결과가 없는 경우(단지 코드가 없음)
+            if (toDB1[0].equals("X")) break;
+
+            String reqBuilder2 = aptBasicInfoUrl +
+                    "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + aptBasicInfoKey + /*Key*/
+                    "&" + URLEncoder.encode("kaptCode", "UTF-8") + "=" + URLEncoder.encode(toDB1[0], "UTF-8"); /*단지코드*/
+
+            // 단지코드를 API에 전달, getDoroJuso의 반환 값은 도로명주소와 법정동주소
+            String[] toDB2 = getDoroJuso(reqBuilder2);
+
+//            // 지도 좌표 API 사용 코드
+//            String reqBuilder2 = vWorldUrl +
+//                    "?" + URLEncoder.encode("key", "UTF-8") + "=" + vWorldKey + /*Key*/
+//                    "&" + URLEncoder.encode("service", "UTF-8") + "=" + URLEncoder.encode("search", "UTF-8") +
+//                    "&" + URLEncoder.encode("request", "UTF-8") + "=" + URLEncoder.encode("search", "UTF-8") +
+//                    "&" + URLEncoder.encode("version", "UTF-8") + "=" + URLEncoder.encode("2.0", "UTF-8") +
+//                    "&" + URLEncoder.encode("crs", "UTF-8") + "=" + URLEncoder.encode("EPSG:4326", "UTF-8") +
+//                    "&" + URLEncoder.encode("size", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8") +
+//                    "&" + URLEncoder.encode("page", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8") +
+//                    "&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("place", "UTF-8") +
+//                    "&" + URLEncoder.encode("format", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8") +
+//                    "&" + URLEncoder.encode("errorformat", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8") +
+//                    "&" + URLEncoder.encode("query", "UTF-8") + "=" + URLEncoder.encode(toDB1[1], "UTF-8"); /*목록 건수*/
+//
+//            // getCoordinate의 반환 값은 도로명 주소, 지번 주소, x,y좌표
+//            String[] toDB2 = getCoordinate(reqBuilder2);
+//
+            toDB.append(Arrays.toString(toDB1));
+            toDB.append(Arrays.toString(toDB2));
+            toDB.append("\n");
+
+            idx++;
+
+            // log.info(Arrays.toString(toDB1) + Arrays.toString(toDB2));
+        }
+        log.info("좌표를 얻지 못한 주소: {}", noneCount);
+
+        // TODO toDB는 DB에 추가하기 위한 형태로 변환이 필요, 1 row = (단지 명, 단지 코드(ID), 도로명 주소, 법정동 주소)
+        // TODO 프론트에서 사용할 수 있도록 각각의 단지 코드에 대해 단지명, 단지코드, 도로명주소, 법정동주소를 가지는 JSON 배열로 변환
+        return toDB.toString();
     }
 
-//    public void aptEnergyAll() throws Exception {
-//        // csv 파일에서 단지 코드 가져옴, 한 단지코드당 202101 ~ 202201까지 탄소 사용량 합계
-//        String kaptCode;
-//        String[] year = { "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022" };
-//        String[] month = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
-//
-//        File aptListAllCsv = new File("src/main/resources/data/aptListAll.csv");
-//        BufferedReader br = null;
-//        String line = "";
-//
-//        try {
-//            br = new BufferedReader(new FileReader(aptListAllCsv));
-//            while ((line = br.readLine()) != null) { // readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
-//                String[] lineArr = line.split(","); // 파일의 한 줄을 ,로 나누어 배열에 저장 후 리스트로 변환한다.
-//                kaptCode = lineArr[0];
-//                for (int i = 0; i < year.length; i++) {
-//                    for (int j = 0; j < month.length; j++) {
-//                        String date = year[i] + month[j];
-//                        String urlBuilder = aptEnergyUrl +
-//                                "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + aptEnergyKey + /*Service Key*/
-//                                "&" + URLEncoder.encode("kaptCode", "UTF-8") + "=" + URLEncoder.encode(kaptCode, "UTF-8") + /*단지코드*/
-//                                "&" + URLEncoder.encode("reqDate", "UTF-8") + "=" + URLEncoder.encode(date, "UTF-8"); /*발생년월*/
-//
-//                    }
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            try {
-//                if (br != null) {
-//                    br.close();
-//                }
-//            } catch(IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+    private String[] getDoroJuso(String reqBuilder) throws Exception {
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(reqBuilder);
+
+        // 제일 첫번째 태그
+        doc.getDocumentElement().normalize();
+
+        // 파싱할 tag
+        NodeList nList = doc.getElementsByTagName("body");
+        Node nNode = nList.item(0);
+        Element eElement = (Element) nNode;
+
+        String kaptAddr = getTagValue("kaptAddr", eElement);
+        String doroJuso = getTagValue("doroJuso", eElement);
+
+        log.info("단지의 법정동주소 : {}, 도로명주소 : {}", kaptAddr, doroJuso);
+
+        return new String[] {kaptAddr, doroJuso};
+    }
+
+    private String[] getCoordinate(String reqBuilder) throws Exception {
+        URL url = new URL(reqBuilder);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        log.info("Response code: {}", conn.getResponseCode());
+
+        BufferedReader rd;
+
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+
+        // log.info(String.valueOf(result));
+        JSONParser parser = new JSONParser();
+        JSONObject objData = (JSONObject) parser.parse(rd);
+        JSONObject response = (JSONObject) objData.get("response");
+        // log.info(String.valueOf(response));
+        JSONObject result = (JSONObject) response.get("result");
+        // 지도 좌표 API로 좌표를 찾을 수 없는 경우
+        if (result == null) {
+            noneCount++;
+            return new String[] {"null"};
+        }
+        // log.info(String.valueOf(result));
+        JSONArray items = (JSONArray) result.get("items");
+        // log.info(String.valueOf(items));
+        JSONObject tmp = (JSONObject) items.get(0);
+        // log.info(String.valueOf(tmp));
+        JSONObject address = (JSONObject) tmp.get("address");
+        // log.info(String.valueOf(address));
+
+        String parcel = (String) address.get("parcel");
+        // log.info(parcel);
+        String road = (String) address.get("road");
+        // log.info(road);
+
+        JSONObject point = (JSONObject) tmp.get("point");
+        String y = (String) point.get("y");
+        // log.info(y);
+        String x = (String) point.get("x");
+        // log.info(x);
+
+        rd.close();
+        conn.disconnect();
+
+        return new String[] {road, parcel, y, x};
+    }
+
+    private String[] getApt(String reqBuilder) throws Exception {
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(reqBuilder);
+
+        // 제일 첫번째 태그
+        doc.getDocumentElement().normalize();
+
+        // 파싱할 tag
+        NodeList nList = doc.getElementsByTagName("body");
+        Node nNode = nList.item(0);
+        Element eElement = (Element) nNode;
+
+        int thisCount = Integer.parseInt(Objects.requireNonNull(getTagValue("pageNo", eElement)));
+        int totalCount = Integer.parseInt(Objects.requireNonNull(getTagValue("totalCount", eElement)));
+
+        // API 호출 결과가 없는 경우
+        if (thisCount > totalCount) return new String[] {"X"};
+
+        // API 호출 결과가 존재, 필요한 값을 추출
+        String[] result = new String[2];
+        result[0] = getTagValue("kaptCode", eElement);
+        result[1] = getTagValue("kaptName", eElement);
+        // result[2] = getTagValue("bjdCode", eElement);
+
+        log.info("단지의 단지 코드 : {}, 단지 명 : {}", result[0], result[1]);
+
+        return result;
+    }
 
     private String callApi(String urlBuilder, String apiName, String date, String attributeList) throws Exception {
         URL url = new URL(urlBuilder);
@@ -121,7 +247,7 @@ public class ApartmentService {
 
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-type", "application/json");
-        System.out.println("Response code: " + conn.getResponseCode());
+        log.info("Response code: {}", conn.getResponseCode());
 
         BufferedReader rd;
 
@@ -141,36 +267,52 @@ public class ApartmentService {
         rd.close();
         conn.disconnect();
 
-        xmlToCsv(sb.toString(), apiName, date, attributeList);
+        // xmlToCsv(sb.toString(), apiName, date, attributeList);
 
         return sb.toString();
     }
 
-    private void xmlToCsv(String xml, String apiName, String date, String attributeList) throws Exception{
-        File stylesheet = new File("src/main/resources/data/" + apiName + "Style.xsl");
+    private String getTagValue(String tag, Element eElement) {
+        //결과를 저장할 result 변수 선언
+        String result = "";
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document document = builder.parse(new InputSource(new StringReader(xml)));
-
-        StreamSource stylesource = new StreamSource(stylesheet);
-        Transformer transformer = TransformerFactory.newInstance()
-                .newTransformer(stylesource);
-        Source source = new DOMSource(document);
-
-        File f = new File("src/main/resources/data/" + apiName + ".csv");
-        BufferedWriter bw;
-
-        if (!f.exists()) {
-            bw = new BufferedWriter(new FileWriter(f));
-            bw.write(attributeList);
-            bw.close();
+        if (eElement.getElementsByTagName(tag).item(0) == null) {
+            noneCount++;
+            return null;
         }
-        bw = new BufferedWriter(new FileWriter(f, true));
-        bw.append(date).append(",");
-        bw.close();
 
-        Result outputTarget = new StreamResult(new FileOutputStream("src/main/resources/data/" + apiName + ".csv", true));
-        transformer.transform(source, outputTarget);
+        NodeList nlList = eElement.getElementsByTagName(tag).item(0).getChildNodes();
+
+        result = nlList.item(0).getTextContent();
+
+        return result;
     }
+
+//    private void xmlToCsv(String xml, String apiName, String date, String attributeList) throws Exception{
+//        File stylesheet = new File("src/main/resources/data/" + apiName + "Style.xsl");
+//
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//        Document document = builder.parse(new InputSource(new StringReader(xml)));
+//
+//        StreamSource stylesource = new StreamSource(stylesheet);
+//        Transformer transformer = TransformerFactory.newInstance()
+//                .newTransformer(stylesource);
+//        Source source = new DOMSource(document);
+//
+//        File f = new File("src/main/resources/data/" + apiName + ".csv");
+//        BufferedWriter bw;
+//
+//        if (!f.exists()) {
+//            bw = new BufferedWriter(new FileWriter(f));
+//            bw.write(attributeList);
+//            bw.close();
+//        }
+//        bw = new BufferedWriter(new FileWriter(f, true));
+//        bw.append(date).append(",");
+//        bw.close();
+//
+//        Result outputTarget = new StreamResult(new FileOutputStream("src/main/resources/data/" + apiName + ".csv", true));
+//        transformer.transform(source, outputTarget);
+//    }
 }
