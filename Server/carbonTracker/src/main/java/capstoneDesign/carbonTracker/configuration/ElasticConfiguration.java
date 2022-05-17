0f34bb51642245fdc6990a7d962f1b744d9dba7c
Go-Jaecheol <gojaech@naver.com>
2022-05-17 21:06:41 +0900

    #30 feature: elasticsearch 조회 기능 구현

---
+++ Server/carbonTracker/src/main/java/capstoneDesign/carbonTracker/configuration/ElasticConfiguration.java
@@ -0,0 +1,35 @@
+package capstoneDesign.carbonTracker.configuration;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@EnableElasticsearchRepositories
+@Configuration
+public class ElasticConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(host)
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
+    }
+}
