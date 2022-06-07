0e4c00df8576c85fec6a450c72e61817ba6508b6
Go-Jaecheol <gojaech@naver.com>
2022-06-07 14:06:57 +0900

    #81 refactoring: 에너지 사용량 예측 API 단일화

--- ML/main.py
+++ ML/main.py
@@ -14,6 +14,10 @@ app = FastAPI()
 load_dotenv()
 
 
+class Item(BaseModel):
+    household: int
+
+
 class ElecItem(BaseModel):
     household: int
     avg_temp: float
@@ -31,15 +35,14 @@ class GasItem(BaseModel):
     avg_wind: float
 
 
-@app.get("/forecast")
-async def forecast():
+def forecast():
     now = datetime.now()
     base_time = now.hour - 1
-    if base_time < 3:
+    if base_time < 5:
         now += timedelta(days=-1)
         base_time = '2300'
     else:
-        base_time = '0200'
+        base_time = '0500'
     base_date = ''.join(str(now.date()).split('-'))
     url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst'
     params = {'serviceKey': environ['FCST_SERVICE_KEY'], 'pageNo': '1', 'numOfRows': '500', 'dataType': 'JSON', 'base_date': base_date,
@@ -59,22 +62,50 @@ async def forecast():
             wsd = float(l.get("fcstValue"))
     tmp = (tmn + tmx) / 2
     result = [tmp, tmx, tmn, reh, wsd]
+    print(response.content)
     return {'avg_temp': tmp, 'max_temp': tmx, 'min_temp': tmn, 'avg_humid': reh, 'avg_wind': wsd}
 
 
+async def elec_predict(item_dict):
+    model = joblib.load('./elec_model.pkl')
+    del(item_dict['avg_wind'])
+    df = pd.DataFrame.from_dict([item_dict])
+    prediction = model.predict(df)
+    return {'elec_predict': int(prediction[0])}
+
+
+async def gas_predict(item_dict):
+    model = joblib.load('./gas_model.pkl')
+    df = pd.DataFrame.from_dict([item_dict])
+    prediction = model.predict(df)
+    return {'gas_predict': int(prediction[0])}
+
+@app.post("/predict/now")
+async def predict_now(item: Item):
+    temp = forecast()
+    print(temp)
+    item_dict = item.dict()
+    item_dict.update(temp)
+    gas_prediction = await gas_predict(item_dict)
+    elec_prediction = await elec_predict(item_dict)
+    temp.update(gas_prediction)
+    temp.update(elec_prediction)
+    return temp
+
+
 @app.post("/predict/elec")
-async def elec_predict(item: ElecItem):
+async def last_elec_predict(item: ElecItem):
     model = joblib.load('./elec_model.pkl')
     item_dict = item.dict()
     df = pd.DataFrame.from_dict([item_dict])
     prediction = model.predict(df)
-    return {'predict': int(prediction[0])}
+    return {'elec_predict': int(prediction[0])}
 
 
 @app.post("/predict/gas")
-async def gas_predict(item: GasItem):
+async def last_gas_predict(item: GasItem):
     model = joblib.load('./gas_model.pkl')
     item_dict = item.dict()
     df = pd.DataFrame.from_dict([item_dict])
     prediction = model.predict(df)
-    return {'predict': int(prediction[0])}
+    return {'gas_predict': int(prediction[0])}
